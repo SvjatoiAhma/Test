6b47ac3f86d42f85a3efdecd18c0fdc314a55ab7
Svjatoslav <svjatoslavmitjagin@gmail.com>
2023-01-26 18:41:10 +0200

    Connect add new project add new 17 40 add new 26.01 1840 add 26.01 2100

--- src/main/java/svjat/test/StartPoint.java
+++ src/main/java/svjat/test/StartPoint.java
@@ -3,25 +3,24 @@ package svjat.test;
 
 public class StartPoint {
     public static void main(String[] args) {
-
         Human man = new Man();
         man.speak();
-
         Human woman = new Woman();
         woman.speak();
 
-      //  System.out.println(man.equals(woman));  тренировка не более того
+        //  System.out.println(man.equals(woman));  тренировка не более того
         boolean manWomen = (man.equals(woman));
         System.out.println(manWomen);
 
         if (man instanceof Human) {
             System.out.println("I'm Human");
         }
-
         if (woman instanceof Man) {
-
         } else {
             System.out.println("I'm not Man");
+        }
+        if (!man.getClass().equals(woman.getClass())){
+            System.out.println("I'm ");
         }
     }
 
