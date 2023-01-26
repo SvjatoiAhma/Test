129d85c1684eabd06f3cc9a3e91a1935524f498a
Svjatoslav <svjatoslavmitjagin@gmail.com>
2023-01-26 18:41:10 +0200

    Connect add new project add new 17 40 add new 26.01 1840

--- src/main/java/svjat/test/Woman.java
+++ src/main/java/svjat/test/Woman.java
@@ -1,9 +1,7 @@
 package svjat.test;
 
-public class Woman extends Human{
-    public void speak(){
+public class Woman extends Human {
+    public void speak() {
         System.out.println("Я говорю на русском");
-
     }
-    //System.out.println("I'm not Man");
 }
